f8948e93399266ec4c3f85f743ad319aac847aeb
AdiSemper <semperag@dukes.jmu.edu>
2020-09-23 23:16:28 -0400

    commit

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const pageNumber = document.querySelectorAll('.position');
 const currnetNav = document.querySelectorAll('.navlink');
 let pageIndex = 0;
-let navIndex = 0;
 
 function forward() {
     if (pageIndex < pageNumber.length - 1) {
         pageNumber[pageIndex].classList.remove('current-page');
+        currnetNav[pageIndex].classList.remove('current-nav');
         pageIndex += 1
         pageNumber[pageIndex].classList.add('current-page');
+        currnetNav[pageIndex].classList.add('current-nav');
     }
 }
 
