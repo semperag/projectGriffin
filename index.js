22a694d1612379d2727312e5b4b8a2529d5d40cc
AdiSemper <semperag@dukes.jmu.edu>
2020-10-09 02:28:16 -0400

    commit

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const pageNumber = document.querySelectorAll('.position');
 const currentNav = document.querySelectorAll('.navlink');
 const currentFrame = document.querySelectorAll('.frame');
 let pageIndex = 0;
+let notAnimating = true;
 
 function forward() {
     if (pageIndex < currentFrame.length - 1) {
-        pageNumber[pageIndex].classList.remove('current-page');
-        currentFrame[pageIndex].classList.remove('current-frame');
-        currentFrame[pageIndex].classList.add('past-frame');
-        currentNav[pageIndex].classList.remove('current-nav');
-        pageIndex += 1
-        pageNumber[pageIndex].classList.add('current-page');
-        currentNav[pageIndex].classList.add('current-nav');
-        currentFrame[pageIndex].classList.add('current-frame');
+
+        if (notAnimating) {
+            pageNumber[pageIndex].classList.remove('current-page');
+            currentFrame[pageIndex].classList.remove('current-frame');
+            currentFrame[pageIndex].classList.add('past-frame');
+            currentNav[pageIndex].classList.remove('current-nav');
+            pageIndex += 1
+            pageNumber[pageIndex].classList.add('current-page');
+            currentNav[pageIndex].classList.add('current-nav');
+            currentFrame[pageIndex].classList.add('current-frame');
+     
+            loading();
+        }
     }
 }
 
@@ -21,8 +27,8 @@ function backward() {
         pageNumber[pageIndex].classList.remove('current-page');
         currentNav[pageIndex].classList.remove('current-nav');
         currentFrame[pageIndex].classList.remove('current-frame');
-        
         pageIndex -= 1
+        currentFrame[pageIndex].classList.remove('way-past-frame');
         pageNumber[pageIndex].classList.add('current-page');
         currentFrame[pageIndex].classList.add('current-frame');
         currentFrame[pageIndex].classList.remove('past-frame');
@@ -40,14 +46,37 @@ function newPage(event) {
     }
 
     if (targetIndex > pageIndex) {
+        pageNumber[pageIndex].classList.remove('current-page');
+        currentFrame[pageIndex].classList.remove('current-frame');
+        currentFrame[pageIndex].classList.add('past-frame');
+        currentNav[pageIndex].classList.remove('current-nav');
+        pageIndex += 1;
+        pageNumber[targetIndex].classList.add('current-page');
+        currentNav[targetIndex].classList.add('current-nav');
+        currentFrame[targetIndex].classList.add('current-frame');
+
+      
         for (i = pageIndex; i < targetIndex; i++) {
-            forward();
+            currentFrame[i].classList.add('way-past-frame');
         }
+        pageIndex = targetIndex
     }
     else if (targetIndex < pageIndex) {
+        pageNumber[pageIndex].classList.remove('current-page');
+        currentNav[pageIndex].classList.remove('current-nav');
+        currentFrame[pageIndex].classList.remove('current-frame');
+        pageIndex -= 1;
+        currentFrame[targetIndex].classList.remove('way-past-frame');
+        pageNumber[targetIndex].classList.add('current-page');
+        currentFrame[targetIndex].classList.add('current-frame');
+        currentFrame[targetIndex].classList.remove('past-frame');
+        currentNav[targetIndex].classList.add('current-nav');
+
         for(i = pageIndex; i > targetIndex; i--) {
-            backward();
+            currentFrame[i].classList.remove('way-past-frame');
+            currentFrame[i].classList.remove('past-frame');
         }
+        pageIndex = targetIndex;
     }
     console.log(pageIndex);
 }
@@ -56,6 +85,12 @@ function contactCalled() {
     for (i = pageIndex; i < currentFrame.length; i++) {
         forward();
     }
+}
+
+function loading(){
+    setTimeout(function(){ 
+        window.notAnimating = false; 
+    }, 5000);
 }
 
 let el = document.getElementById('arrow-down');
